a2ee34f8241c58b8cfc876def9495e8403a37a77
Astral Gnome <reedkeenan@gmail.com>
2023-01-24 15:07:33 -0800

    Reinstalled a couple of things and started working on the NavBar.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import NavBar from "../ordinary_components/NavBar"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
