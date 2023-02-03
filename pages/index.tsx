151fb92fb828038949bfa57db42dc2deba6f6f6c
Astral Gnome <reedkeenan@gmail.com>
2023-02-02 21:33:29 -0800

    This is the last test, for sure!

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,17 @@ export default function Home() {
           content={`An open-source project to service Kusama's chain data to the Ethereum network via a Chainlink external adapter.`}
         />
         <meta property='og:image:alt' content='Project Gweihir thumbnail image' />
+        {/* TWITTER */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Project Gweihir' />
+        <meta
+          name='twitter:description'
+          content={`An open-source project to service Kusama's chain data to the Ethereum network via a Chainlink external adapter.`}
+        />
+        <meta
+          name='twitter:image'
+          content='https://website-one-delta-18.vercel.app/Images/epic_thumbnail.png'
+        />
       </Head>
       <NavBar />
       <HomePage />
