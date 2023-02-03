756019eb8b5f7b601b5d382c20a43856f6d376d9
Astral Gnome <reedkeenan@gmail.com>
2023-02-02 21:52:56 -0800

    The final last test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta property='og:title' content='Project Gweihir' />
+        <meta property='og:title' content='Project Gweihir, Expanding What is Possible' />
         <meta
           property='og:image'
           content='https://website-one-delta-18.vercel.app/Images/epic_thumbnail.png'
@@ -16,7 +16,7 @@ export default function Document() {
         <meta property='og:image:alt' content='Project Gweihir thumbnail image' />
         {/* TWITTER */}
         <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='Project Gweihir' />
+        <meta name='twitter:title' content='Project Gweihir, Expanding What is Possible' />
         <meta
           name='twitter:description'
           content={`An open-source project to service Kusama's chain data to the Ethereum network via a Chainlink external adapter.`}
