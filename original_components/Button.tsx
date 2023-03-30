b0605a046e1f887d9c2cdfb13127aa59d6acc519
Astral Gnome <reedkeenan@gmail.com>
2023-03-29 22:45:39 -0700

    change

--- original_components/Button.tsx
+++ original_components/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({ buttonText, onClick, className = "", di
   <button
     onClick={onClick}
     disabled={disabled}
-    className={`py-2 font-normal cursor-pointer px-4 text-sm sm:text-base rounded-lg text-black bg-primary ${
+    className={`py-2 font-semibold cursor-pointer px-4 text-sm sm:text-base rounded-lg text-slate-700 bg-primary ${
       disabled && "bg-gray-400"
     } ${!disabled && "hover:bg-accent"} ${!disabled && "active:bg-thirdinary"} ${className}`}
   >
--- pages/home.tsx
+++ pages/home.tsx
@@ -19,12 +19,12 @@ export default function HomePage(props: Props) {
         width={500}
         src={epicContent}
         alt='Image'
-        className='invisible lg:visible mb-4 lg:w-1/3 xl:w-fit xl:mr-44 lg:mr-6 md:mr-48 mt-60 absolute lg:top-10 md:top-56 top-10 xl:top-0 right-0 opacity-100 animate-fade_in'
+        className='invisible lg:visible mb-4 lg:w-1/3  xl:mr-44 lg:mr-6 md:mr-48 mt-60 absolute lg:top-10 md:top-56 top-10 xl:top-0 right-0 opacity-100 animate-fade_in'
       />
       <div className='flex-1 py-8 px-6 relative'>
         <section
           id='Home'
-          className='mt-10 md:mt-44 sm:pr-24 ml-0 md:ml-7 lg:ml-18 xl:ml-36 lg:w-2/3 xl:w-5/12 w-full'
+          className='mt-10 md:mt-44 sm:pr-24 ml-0 md:ml-7 lg:ml-18 xl:ml-36 lg:w-2/3 xl:w-5/12  w-full'
         >
           <h1 className='text-white text-2xl sm:text-4xl font-medium mb-4 text-left pl-4 pb-4 sm:pb-0'>
             Bridge Kusama Data
