a4a44a647943607a8816350e6f75c345bc1a9f36
Astral Gnome <reedkeenan@gmail.com>
2023-04-03 17:35:43 -0700

    Team page ready for deploy as soon as Tyler's info arrives

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,66 +1,89 @@
-import Card from "@/original_components/Card"
-import React from "react"
+import React, { useState, MouseEventHandler } from "react"
 import Image from "next/image"
+import AdamHeadshot from "../public/Images/Team/AdamHeadshot.png"
+import SpencerHeadshot from "../public/Images/Team/SpencerHeadshot.png"
+import SpiessHeadshot from "../public/Images/Team/SpiessHeadshot.png"
+import KeenanHeadshot from "../public/Images/Team/KeenanHeadshot.png"
+import AndreaHeadshot from "../public/Images/Team/AndreaHeadshot.png"
+import TylerHeadshot from "../public/Images/Team/blank-profile-picture.png"
 
 const TeamArray = [
   {
-    name: `Adam`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Adam Steeber`,
+    title: "Founder",
+    bio: "Adam has been an active member in the Kusama ecosystem since April of 2021 and has a bachelor's degree in economics. He runs a validator node on Kusama and is interested in realizing the Web3 paradigm. His focuses are interoperability and solving real-world problems using blockchain technology.",
+    headshot: AdamHeadshot,
   },
 
   {
-    name: `Spencer`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Spencer Brown`,
+    title: "Lead Developer & Project Engineer",
+    bio: "Spencer is a software engineer specializing in full-stack web development with a bachelor's degree in software engineering from Arizona State University.",
+    headshot: SpencerHeadshot,
   },
   {
-    name: `Nick`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Nick Spiess`,
+    title: "Marketing Coordinator",
+    bio: "Nick is a software developer and has been active in the blockchain ecosystem since 2017.  He is passionate about Web3 and the potential it holds to create a more equitable world.",
+    headshot: SpiessHeadshot,
   },
   {
-    name: `Keenan`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Keenan Reed`,
+    title: "Associate Developer & Frontend Director",
+    bio: "Keenan is a frontend web developer with a background in writing, editing, and visual design. Recently, his focus has been shifting toward dApps and the Web3 ecosystem.",
+    headshot: KeenanHeadshot,
   },
   {
-    name: `Andrea`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Andrea Vendrame`,
+    title: "Full-stack dApp & Solidity developer",
+    bio: "Andrea is a passionate software engineer specializing in full-stack web development with a master's degree in Design and Development of Software Systems from the University of Udine (Italy).",
+    headshot: AndreaHeadshot,
   },
   {
-    name: `The Other`,
-    bio: "Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+    name: `Tyler`,
+    title: "",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    headshot: TylerHeadshot,
   },
 ]
 
 type Props = {}
 
 export default function Team(props: Props) {
+  const [readMore, setReadMore] = useState(Array(TeamArray.length).fill(false))
+
+  const handleReadMore: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const index = Number(event.currentTarget.getAttribute("data-index"))
+    setReadMore((prevState) => {
+      const newState = [...prevState]
+      newState[index] = !newState[index]
+      return newState
+    })
+  }
+
   return (
-    <main className='w-3/5 h-auto mx-auto bg-slate-700 pb-16 mb-10 mt-20'>
-      <div id='team' className='flex flex-wrap justify-around h-auto pb-10 mt-10'>
-        <h1 className='font-lg w-full text-accent font-semibold text-center mt-6 text-3xl'>Team</h1>
-        <div className='flex flex-wrap justify-center w-full'>
+    <main className='bg-slate-700'>
+      <div className='w-4/5 mx-auto mt-20 pt-4 pb-0'>
+        <h1 className='font-semibold text-3xl text-center mt-6 text-accent'>Team</h1>
+        <div id='team' className='flex flex-wrap justify-center pt-10 pb-12'>
           {TeamArray.map((team, index) => (
             <div
               key={index}
-              className={`border-slate-700 bg-slate-800 w-46 h-1/2 items-left justify-evenly xl:m-6 lg:m-4 m-2 p-6 rounded-md border-2`}
+              className='w-46 h-1/2 m-2 px-6 pt-6 pb-4 border-2 rounded-md border-slate-600 bg-slate-800'
             >
-              <div className='mb-4 flex-row justify-content-between'>
-                <div className='flex items-center'>
-                  <div>
-                    <Image
-                      src='/../public/Images/Birdie_02.png'
-                      height={60}
-                      width={60}
-                      alt='A face'
-                    />
-                  </div>
-                  <div>
-                    <h1 className={`text-accent text-xl font-semibold text-left pb-3 ml-2 w-40`}>
-                      {team.name}
-                    </h1>
-                  </div>
+              <div className='mb-4 flex items-center'>
+                <Image src={team.headshot} alt='A face' width={60} height={60} className='ml-0' />
+                <div className='flex flex-col'>
+                  <h1 className='ml-6 text-accent text-lg font-semibold'>{team.name}</h1>
+                  <h1 className='ml-6 text-accent text-sm font-normal w-40 h-10'>{team.title}</h1>
                 </div>
-                <p className={`text-gray-300 text-left line-clamp-10 ml-2 w-56`}>{team.bio}</p>
               </div>
+              <p className={`mt-4 text-gray-300 w-80 ${readMore[index] && "line-clamp-3"}`}>
+                {team.bio}
+              </p>
+              <button className='text-accent pt-1' onClick={handleReadMore} data-index={index}>
+                {readMore[index] ? "-read more-" : "-close-"}
+              </button>
             </div>
           ))}
         </div>
